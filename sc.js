409c4f53d51349431a9491cb072528348d415624
anarazin <anaabdullah77777@gmail.com>
2020-09-27 06:57:56 +0600

    added index.html

---
+++ sc.js
@@ -0,0 +1,33 @@
+const title= document.querySelector("#title");
+const author= document.querySelector("#a");
+const year= document.querySelector("#y");
+const btn= document.querySelector(".btn");
+const booklist= document.querySelector("#book-list");
+
+btn.addEventListener("click",function(p){
+    p.preventDefault();
+
+    if(title.value =="" , author.value=="" , year.value==""){
+        alert("kisu to lekhen!");
+    } else{
+        const newRow= document.createElement("tr");
+        const newTitle= document.createElement("th");
+        newTitle.innerHTML=title.value;
+
+        newRow.appendChild(newTitle);
+
+        const newAuthor= document.createElement("th");
+        newAuthor.innerHTML=author.value;
+
+        newRow.appendChild(newAuthor);
+
+        const newYear= document.createElement("th");
+        newYear.innerHTML=year.value;
+
+        newRow.appendChild(newYear);
+
+        booklist.appendChild(newRow);
+        
+
+    }
+})
